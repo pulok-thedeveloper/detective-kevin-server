f066cb9a463f5afd15871d9aeb1975efee2b67a5
pulok-thedeveloper <mdpulokhasan@gmail.com>
2022-11-10 02:32:01 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
+// middle wares
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.tftz42f.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
+app.get('/', (req,res) =>{
+    res.send('DETECTIVE KEVIN server is running')
+})
+
+app.listen(port, ()=>{
+    console.log(`Detective Kevin server running on ${port}`);
+})
